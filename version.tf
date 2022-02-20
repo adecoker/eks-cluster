652d84ad836e85f410ef1f390ada6740b1eee144
Ade Coker <88948587+adecoker@users.noreply.github.com>
2022-02-20 14:30:14 -0800

    Create version.tf

---
+++ version.tf
@@ -0,0 +1,11 @@
+terraform {
+    required_providers {
+        randon = {
+            source = "hashicorp/randon"
+            version = "3.0.0"
+        }
+        aws = {
+            source = "hashicorp/aws"
+            version = "1.16.0"
+        }
+}
